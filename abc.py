555bccb957decabea2fa763a183c948f0e0b3345
root <root@ip-172-31-92-51.ec2.internal>
2018-09-20 17:22:24 +0000

    changes saved

--- abc.py
+++ abc.py
@@ -2,11 +2,18 @@
 
 this is my change demo
 
+<<<<<<< HEAD
 this is for stashing demo
+=======
+
+>>>>>>> feature2
 
 this is  feature1
 
 this the change in feature2
+<<<<<<< HEAD
 
 
 stast saved
+=======
+>>>>>>> feature2
